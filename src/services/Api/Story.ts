302ed30e03b73abff31ae56dcb04a613a169d420
Bqss1 <basofi.cucokmeong12@gmail.com>
2024-01-16 22:25:39 +0700

    [fix] error build

--- src/services/Api/Story.ts
+++ src/services/Api/Story.ts
@@ -1,16 +1,7 @@
 class Story{
   static async getAllStory(params : {params : }): Promise<Story> {
-    const response = await axios.get("http://localhost:3000/api/story" + paramify({
-      search: searchParams,
-      category,
-      status
-    })).then(res => {
-      return res.data.data;
-    }).catch(err => {
-      console.log(err);
-    }),;
-    const story = await response.json();
-    return story;
+
+
   }
 }
 
